9ea9a18df78a61d53b0e02feb13d042f429d5f7f
vijuprakash <vijuprakash@rajagiritech.edu.in>
2017-03-06 14:36:49 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter two values here");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("Sum of %d and %d is %d", a, b, c);
+getch();
+}
